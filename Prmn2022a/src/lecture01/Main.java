cfaf6d0af3ff7e4b43a1cc7e8b62796ba59d01d3
b2211780 <b2211780@photon.chitose.ac.jp>
2022-10-31 18:07:18 +0900

    プロジェクト作成

--- Prmn2022a/src/lecture01/Main.java
+++ Prmn2022a/src/lecture01/Main.java
@@ -3,6 +3,7 @@ package lecture01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("B2211780 Ryoga nakamura");
-
+        int a = 2211780;
+        System.out.println("B"+ a +" Ryoga nakamura");
     }
 }
