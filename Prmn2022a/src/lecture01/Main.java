13d7b1780a2107263cf1040164436f76ccd6a4e1
b2211780 <b2211780@photon.chitose.ac.jp>
2022-10-31 18:18:49 +0900

    プロジェクト作成

--- Prmn2022a/src/lecture01/Main.java
+++ Prmn2022a/src/lecture01/Main.java
@@ -1,9 +1,21 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("B2211780 Ryoga nakamura");
+
         int a = 2211780;
         System.out.println("B"+ a +" Ryoga nakamura");
+
+        Scanner  input = new Scanner(System.in);
+        int age = input.nextInt();
+        if(age < 20){
+            System.out.println("I am "+age+ " years old so I cannot drink liquor");
+        }else {
+            System.out.println("I am "+age+" years old so I can drink liquor.");
+        }
+
     }
 }
