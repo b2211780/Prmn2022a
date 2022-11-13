069472ce6f5c6d5493acdb5d4b54032fe1aee708
b2211780 <b2211780@photon.chitose.ac.jp>
2022-11-14 00:13:43 +0900

    Exercise3_2

--- Prmn2022a/src/lecture3/Exercise3_2.java
+++ Prmn2022a/src/lecture3/Exercise3_2.java
@@ -1,7 +1,27 @@
 package lecture3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercise3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("何行分入力しますか?");
+        System.out.print(">");
+        int gyou = scanner.nextInt();
+
+        ArrayList<String> moji = new ArrayList<>();
+        for(int i=0;i<gyou;i++){
+            System.out.println((i+1)+"行目:");
+            System.out.print(">");
+            String a = scanner.next();
+            moji.add(a);
+        }
+        System.out.println("入力した文字列:");
+        for(int i=0;i<gyou;i++) {
+            System.out.println("["+i+"]" + moji.get(i));
+        }
+    }
+
 
 }
