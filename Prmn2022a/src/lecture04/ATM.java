06f9c65064b709aa22f9287b242b2f7981f13766
b2211780 <b2211780@photon.chitose.ac.jp>
2022-11-21 00:40:57 +0900

    Exercise4_2

---
+++ Prmn2022a/src/lecture04/ATM.java
@@ -0,0 +1,60 @@
+package lecture04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ATM {
+    List<Account> accountList;
+
+    public ATM() {
+        this.accountList = new ArrayList<Account>();
+    }
+
+    public void registerAccount(String name, String number) {
+        Account ac = new Account(name, number);
+        if (existAccount(name, number)) {
+            System.out.println("名前:" + name + " 口座番号:" + number + "は存在します");
+        } else {
+            accountList.add(ac);
+            System.out.println(name + "さんのアカウントを、口座番号:" + number + "で登録しました");
+        }
+    }
+
+    public boolean existAccount(String name, String number) {
+        for (Account account : accountList) {
+            if (account.getName().equals(name) && account.getNumber().equals(number)) {
+                System.out.println("名前:" + name + " 口座番号:" + number + "は存在します");
+                return true;
+            }
+        }
+        System.out.println("名前:" + name + " 口座番号:" + number + "は存在しません");
+        return false;
+    }
+
+    public void deposit(String number, long money) {
+        for (Account account : accountList) {
+            if (account.getNumber().equals(number)) {
+                account.setBalance(money);
+                System.out.println("口座番号: " + number + " に " + money + " 円入金しました");
+            }
+        }
+    }
+
+    public long withdraw(String number, long money) {
+        for (Account account : accountList) {
+            if (account.getNumber().equals(number) && account.getBalance() > money) {
+                long kane = account.getBalance() - money;
+                System.out.println("口座番号: " + number + " から" + money + " 円引き出しました。　残高:" + kane+"　円");
+                return kane;
+            } else {
+                System.out.println("口座番号:" + number + " から" + money + " 円引き出せませんでした。　残高:" + account.getBalance()+"　円");
+            }
+        }
+        return accountList.get(0).getBalance();
+
+    }
+
+}
+
+
+
---
+++ Prmn2022a/src/lecture04/Exercise4_2.java
@@ -0,0 +1,14 @@
+package lecture04;
+
+public class Exercise4_2 {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+
+
+        atm.registerAccount("中村涼雅","2211780");
+        atm.existAccount("中村涼雅","2211780");
+        atm.deposit("2211780",1000);
+        atm.withdraw("2211780",2000);
+        atm.withdraw("2211780",500);
+    }
+}
